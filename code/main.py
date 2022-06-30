f4110611002cc3dbf191b00eb3668acb1a294327
Artic <81034458+ArticOff@users.noreply.github.com>
2022-06-30 19:42:48 +0200

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
-from discord_slash.utils.manage_components import *
+from discord_slash.utils.manage_components import * # évite de faire ça
 from discord_slash.utils.manage_components import create_choice, create_option
 
 from time import sleep
