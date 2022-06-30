0ede3f1b28bba027ba4677e75cdbcf675b10da0b
Linajuci <102174489+Elpouletto@users.noreply.github.com>
2022-06-30 19:37:32 +0200

    Update main.py
    J'ai documenté les fonctions/classes.

--- code/main.py
+++ code/main.py
@@ -6,14 +6,32 @@ from discord_slash.utils.manage_components import create_choice, create_option
 
 from time import sleep
 
-prefix = ""
+prefix = "/"
 client = commands.Bot(command_prefix=prefix)
 client.remove_command('help')
 slash = SlashCommand(client, sync_commands=True)
 help_commands = {"mod":[], "fun":[], "use":[]}
 
+def help_append(type, name, value):
+	"""
+	This function will add a command to the Help Message.
+	
+	Use it like that : help_append(type, name, value)
+	
+	type -> from wich category is the command (mod/fun/use)
+	name -> the name of your command
+	value -> The description of your command
+	"""
+	help_commands[type].append([name, value])
+
 
 class MyEmbed:
+	"""
+	This class has the principals discord.Embed functions and more things :
+		- It say on the top of the Embed who executed the command;
+		- It put a rainbow bar at the end and say the credits
+	"""
+	
 	def __init__(self, ctx, title, description, color=discord.Color.dark_blue()):
 		self.embed = discord.Embed(title=title, description=description, color=color)
 		self.embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
@@ -32,8 +50,18 @@ class MyEmbed:
 	def set_image(self, url):
 		self.embed.set_image(url=url)
 
-
+		
 async def send_error(ctx, error):
+	"""
+	This function is used for send formated error messages !
+	
+	Use it like that : await send_error(ctx, error)
+	
+	ctx -> The context;
+	error -> Your error (Ex. This player isn't on this server !)
+	
+	It will delete by his self in 3 seconds.
+	"""
 	embed = discord.Embed(title=f":x: | {error}", color=discord.Color.dark_red())
 	await ctx.message.delete()
 	msg = await ctx.send(embed=embed)
@@ -42,13 +70,10 @@ async def send_error(ctx, error):
 
 
 @client.event
-async def on_command_error(ctx, error):
-	if isinstance(error, commands.CommandNotFound):
-		await send_error(ctx, "Commande inexistante !")
-
-
-@client.event
 async def on_ready():
+	"""
+	This function say Ready ! In the terminal when it's loaded and make the Bot status to Online.
+	"""
 	await client.change_presence(status=discord.Status.online, activity=discord.Game(f'{prefix}help'))
 	print("Ready !")
 
