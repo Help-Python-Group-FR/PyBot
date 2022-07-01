8bcb1eadc4014df216ff0ae224224b92ea515621
Linajuci <102174489+Elpouletto@users.noreply.github.com>
2022-07-01 15:44:56 +0200

    Update mod.py

--- code/mod.py
+++ code/mod.py
@@ -3,3 +3,7 @@ from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_components import * # évite de faire ça
 from discord_slash.utils.manage_components import create_choice, create_option
+
+prefix = "/"
+bot = commands.Bot(command_prefix=prefix)
+
