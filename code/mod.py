c7ed010e7c1a31cf308c8d512b6f6e682290d16e
Linajuci <102174489+Elpouletto@users.noreply.github.com>
2022-07-01 15:56:51 +0200

    Update mod.py

--- code/mod.py
+++ code/mod.py
@@ -4,6 +4,8 @@ from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_components import * # évite de faire ça
 from discord_slash.utils.manage_components import create_choice, create_option
 
-prefix = "/"
-bot = commands.Bot(command_prefix=prefix)
+from main.py import bot, slash, prefix, send_error, MyEmbed
+from main.py import help_append as ha
 
+def help_append(name, value):
+  ha("mod", name, value)
