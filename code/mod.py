a12c5b19393fa2f49cc1297ba43328ef477ae66d
Linajuci <102174489+Elpouletto@users.noreply.github.com>
2022-07-01 15:40:29 +0200

    Update mod.py

--- code/mod.py
+++ code/mod.py
@@ -1,2 +1,5 @@
 import discord
-from discord.utils import commands
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_components import * # évite de faire ça
+from discord_slash.utils.manage_components import create_choice, create_option
