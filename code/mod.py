41a59ebdd367a4108c5caf88e789fc3416756d50
Linajuci <102174489+Elpouletto@users.noreply.github.com>
2022-07-01 16:00:55 +0200

    Update mod.py

--- code/mod.py
+++ code/mod.py
@@ -4,7 +4,7 @@ from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_components import * # évite de faire ça
 from discord_slash.utils.manage_components import create_choice, create_option
 
-from main.py import bot, slash, prefix, send_error, MyEmbed
+from main import bot, slash, prefix, send_error, MyEmbed
 
 help_commands = []
 
