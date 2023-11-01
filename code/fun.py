16a2aed84e9c66aafc6468f616878628c65af595
Linajuci <juliandellacasa@icloud.com>
2023-11-01 11:29:33 +0100

    Je finis de Setup les fichiers pour définir les commandes

--- code/fun.py
+++ code/fun.py
@@ -1,7 +1,14 @@
 import discord
 from discord import app_commands
 
-async def first_test(interaction : discord.Interaction):
-    await interaction.response.send_message("Hello World !")
-
-fun_commands = [{'type':'default', 'name':'deuxième-test', 'description':'Hello World !', 'func':first_test}]
+# Cette ligne doit se trouver à la fin du fichier !
+fun_commands = []
+# Elle sert à ajouter à "l'arbre" des commandes du Bot la commande crée :
+#
+# Pour cela il faut y mettre un nouveau dictionnaire et y mettre les clés suivantes avec ce qui leur correspond en paramètre :
+#   - 'name' : Le nom de ta commande (Ex. say-goodbye)
+#   - 'description' : La description de ta commande (Ex. Never Gonna Give You Up !)
+#   - 'func' : la fonction qui contient ta commande (Ex. say_goodbye_command) PS. Ne pas mettre les parenthèses
+#
+# Ex. fun_commands = [{'name':'deuxième-test', 'description':'Hello World !', 'func':first_test},
+#                     {'name':'salut', 'description':'Te dis salut !', 'func':hi_command}]
--- code/main.py
+++ code/main.py
@@ -1,6 +1,10 @@
 import discord
 from discord import app_commands
+
+# Ici j'importe les fichiers où l'on a défini les commandes
 import fun
+import mod
+import utils
 
 # Je définis les variables essentiels du Bot ici :
 intents = discord.Intents.default()
@@ -9,6 +13,12 @@ tree = app_commands.CommandTree(client)
 
 # Ici j'ajoute les commandes définient dans les autres fichiers :
 for command in fun.fun_commands:
+    tree.command(name=command['name'], description=command['description'])(command['func'])
+
+for command in mod.mod_commands:
+    tree.command(name=command['name'], description=command['description'])(command['func'])
+
+for command in utils.utils_commands:
     tree.command(name=command['name'], description=command['description'])(command['func'])
 
 
@@ -23,6 +33,20 @@ async def on_ready():
     await client.change_presence(activity=discord.Game("/game"), status=discord.Status.online)
     print(f"We have logged in as {client.user}")
 
-TOKEN = "OTIxMTQ1MzU1NTYxNzQyMzk2.GygPpQ.O30WxZlYFqXG7W-SVKZiBreQS1CSsERP2VYUOY"
+
+@tree.error
+async def on_command_error(interaction, error):
+    """
+    Cette commande s'éxecute lorsque le Bot reçoit une erreur :
+        - Pour l'instant elle ne traite que les erreurs de permissions;
+    """
+    if isinstance(error, app_commands.MissingPermissions):
+        embed = discord.Embed(title=":x: | Tu n'as pas la permission d'utiliser cette commande !", color=discord.Color.red())
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+        return
+
+    raise error
+
+TOKEN = "OTIxMTQ1MzU1NTYxNzQyMzk2.GwXQOU.XkunSeQwwLGOoZOJLYwYDFd1Q91amN-ASxutDE"
 print("Launch of the Client...")
 client.run(TOKEN)
